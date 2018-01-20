8ba09262b9695cb3701dafdde1263764794f0711
Colton Loewen <1569808+cloewen8@users.noreply.github.com>
2018-01-19 20:37:47 -0700

    Listened to http port

--- bot.js
+++ bot.js
@@ -1,5 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const http = require("http");
+let server;
+
+function handleRequest(request, response) {
+	response.setHeader("Location", "https://github.com/scoobycode/scam-bot");
+	response.statusCode = 307;
+	response.end();
+}
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -13,3 +21,12 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+
+server = http.createServer(handleRequest);
+server.listen(process.env.PORT || 8080, (err) => {
+	if (err) {
+		return console.error(err);
+	}
+
+	console.log(`Server started on port ${process.env.PORT || 8080}`);
+});
