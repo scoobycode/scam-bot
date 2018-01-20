831024e15c038d968332131792f382faff2332f8
Colton Loewen <loewencjay@gmail.com>
2018-01-20 02:56:54 -0700

    Cleanup

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const http = require("http");
+const http = require('http');
 let server;
 
 function handleRequest(request, response) {
@@ -19,7 +19,6 @@ client.on('message', message => {
   	}
 });
 
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
 
 server = http.createServer(handleRequest);
