854ecd12b042a2a5723d38a1a26ca06568060b2b
scoobycode <35615829+scoobycode@users.noreply.github.com>
2018-02-09 20:14:23 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+const botconfig = require("./botconfig.js");
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
+
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f,i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+});
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity("ethanlaj", {type: "WATCHING"});
+});
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+if(!message.content.startsWith(botconfig.prefix)) return;
+let commandfile = bot.commands.get(cmd.slice(prefix.length));
+if(message.author.id === "245877990938902529") return commandfile.run(bot, message, args);
+
+})
+
+
+bot.login(botconfig.token);
