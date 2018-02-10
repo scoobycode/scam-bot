4943c50c43a1860831db26c00a053075b6e07264
scoobycode <35615829+scoobycode@users.noreply.github.com>
2018-02-09 20:07:27 -0800

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,10 @@
+
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+
+    message.channel.send("pong")
+    
+}
+module.exports.help = {
+    name: "ping"
+}
